31179fb4663bc6433090abe136dcbaedebbd82f3
Dace Willmott <willmott@Brooklyns-iMac-4.local>
2015-07-29 10:22:06 -0400

    use es6 class to contain carousel

--- app/index.js
+++ app/index.js
@@ -2,48 +2,41 @@
 
 import StyleSheet from './index.styl';
 
-// images array
-let imageSlideshow = ["place_01.jpg", "place_02.jpg", "place_03.jpg", "place_04.jpg", "place_05.jpg"];
-
-// load images onto the page
-function addImages(){
-  imageSlideshow.forEach(function(image){
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = `<img src=../assets/${image} class=images>`;
-    document.getElementById("carousel").appendChild(newDiv);
-  });
-}
-
-addImages();
-
-
-// ============= MOVE IMAGES =================================
-
-var currentIndex = 0;
-
-var nextButton = document.getElementById('next');
-var prevButton = document.getElementById('prev');
-
-var moveImage = function() {
-  $("#carousel").velocity({
-    translateX: `${currentIndex * -800}px`
-  }); 
- }
-
-nextButton.onclick = function(){
-  currentIndex++;
-  moveImage();
-};
-
-prevButton.onclick = function(){
-  currentIndex--;
-  moveImage();
-};
-
 
 class Carousel {
-  constructor (imageSlideshowArray) {
-    this.imageSlideshowArray = imageSlideshowArray;
+  constructor (imageSlideshow) {
+    this.imageSlideshow = imageSlideshow; 
+  }
+
+  renderImages(){
+    this.imageSlideshow.forEach(function(image){
+      let newDiv = document.createElement('div');
+      newDiv.innerHTML = `<img src=../assets/${image} class=images>`;
+      document.getElementById("carousel").appendChild(newDiv);
+    });
+  }
+
+  buttonControls(){  
+    
+    let currentImageIndex = 0;
+    let nextButton = document.getElementById('next');
+    let prevButton = document.getElementById('prev');
+    
+    function moveImages() {
+      $("#carousel").velocity({
+        translateX: `${currentImageIndex * -800}px`
+      }); 
+    }
+
+    nextButton.onclick = function(){
+      currentImageIndex++;
+      moveImages();
+    };
+
+    prevButton.onclick = function(){
+      currentImageIndex--;
+      moveImages();    
+    };
   }
 
 
@@ -51,8 +44,14 @@ class Carousel {
 
 
 
+let imagesArray = ["place_01.jpg", "place_02.jpg", "place_03.jpg", "place_04.jpg", "place_05.jpg"];
 
-let CreateCarousel = new Carousel;
+
+
+let TravelCarousel = new Carousel(imagesArray);
+TravelCarousel.renderImages();
+TravelCarousel.buttonControls();
+
 
 
 
